15ac7e624209c64c42fadc7fb512171ced965923
jwerner54 <jacob.f.werner@comcast.net>
2021-04-30 11:37:36 -0500

    angular version update

--- src/app/mentor-matching/mentor-matching.component.spec.ts
+++ src/app/mentor-matching/mentor-matching.component.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
@@ -9,7 +9,7 @@ describe('MentorMatchingComponent', () => {
   let component: MentorMatchingComponent;
   let fixture: ComponentFixture<MentorMatchingComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MentorMatchingComponent ]
     })
